4d540e9f74733a5b9b57b3b6bd456fbf7cf58e73
Ninjinha64 <95583856+Ninjinha64@users.noreply.github.com>
2022-01-17 15:00:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+function preload(){
+  //imagens pré-carregadas
+
+  loadAnimation("Runner-1.png","Runner-2.png,path.png");
+
+groudImage = ("path.png");
+
+ground.add
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  //crie sprite aqui
+}
+
+function draw() {
+
+
+  background("black");
+
+ groud,velocityX = -2;
+
+
+if (ground.x < 0 ) {
+ground.x = ground.widht / 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
